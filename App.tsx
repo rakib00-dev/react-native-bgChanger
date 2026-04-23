a6a36cc063517ceec0e6d4e995ff56b291cfcebe
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2026-04-24 00:15:41 +0600

    updated the functionality

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import {
+  ScrollView,
   StatusBar,
   StyleSheet,
   Text,
@@ -6,6 +7,7 @@ import {
   View,
 } from 'react-native';
 import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   const [randomBackground, setRandomBackground] = React.useState('#ffffff');
@@ -21,21 +23,46 @@ export default function App() {
     setRandomBackground(colorHex);
   };
   return (
-    <>
-      <StatusBar backgroundColor={'#ffffff'} />
-      <View style={[styles.container]}>
-        <TouchableOpacity onPress={generateColor}>
-          <View style={styles.actionBtn}>
-            <Text style={styles.actionBtnTxt}>Press Me</Text>
-          </View>
-        </TouchableOpacity>
-      </View>
-    </>
+    <SafeAreaView>
+      <ScrollView>
+        <StatusBar backgroundColor={randomBackground} />
+        <View style={[styles.container]}>
+          <TouchableOpacity onPress={generateColor}>
+            <View style={styles.actionBtn}>
+              <Text style={styles.actionBtnTxt}>Press Me</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
-  actionBtn: {},
-  actionBtnTxt: {},
+  statusBarBody: {
+    height: 100,
+    width: 100,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+    flexDirection: 'column',
+  },
+  actionBtn: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 350,
+  },
+  actionBtnTxt: {
+    color: 'Black',
+    borderColor: 'black',
+    borderWidth: 4,
+    backgroundColor: 'white',
+    borderRadius: 30,
+    padding: 10,
+    fontSize: 28,
+  },
 });
